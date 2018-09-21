c12612761b5c8a4d934dbcba2a721a99a6f9845e
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2018-09-21 14:59:21 +0200

    Made it work. Each button lights its own LED

--- exercise 1/ex1.s
+++ exercise 1/ex1.s
@@ -82,8 +82,46 @@
 	      .type   _reset, %function
         .thumb_func
 _reset: 
-	      b .  // do nothing
+	//setup GPIO clock in CMU
+	ldr r1, =CMU_BASE
+	ldr r2, =CMU_HFPERCLKEN0
+	ldr r3, =CMU_HFPERCLKEN0_GPIO
+	mov r4, 1
+	lsl r4, r4, r3
+	orr r4, r4, r2
+	str r4, [r1, r2]
 	
+	//Setup port a
+	ldr r1, =GPIO_PA_BASE
+	mov r2, 0x2
+	str r2, [r1]
+	ldr r2, =GPIO_MODEH
+	mov r3, 0x55555555
+	str r3, [r1, r2]
+	//Turn off lights
+	ldr r2, =GPIO_DOUT
+	mov r3, 0xffffffff
+	str r3, [r1, r2]
+	
+	//Setup port c
+	ldr r1, =GPIO_PC_BASE
+	ldr r2, =GPIO_MODEL
+	mov r3, 0x33333333
+	str r3, [r1, r2]
+	ldr r2, =GPIO_DOUT
+	mov r3, 0xff
+	str r3, [r1, r2]
+	
+	ldr r1, =GPIO_PA_BASE
+	ldr r2, =GPIO_PC_BASE
+	ldr r3, =GPIO_DIN
+	ldr r4, =GPIO_DOUT
+	loop:
+		ldr r5, [r2, r3]
+		lsl r5, r5, 8
+		str r5, [r1, r4]
+		b loop
+
 	/////////////////////////////////////////////////////////////////////////////
 	//
   // GPIO handler
@@ -102,52 +140,4 @@ gpio_handler:
 dummy_handler:  
         b .  // do nothing
 
-/*This section is for the setup of the GIOP*/
-//Enable CMU clock
-ldr r1, =CMU_BASE
-ldr r2, =CMU_HFPERCLKEN0
-mov r3, 0b10000000000000
-str r3, [r2, r1]
-//Setup for port C
-ldr r1, =GPIO_PC_BASE
-ldr r2, =GPIO_MODEL
-mov r0, 0x33333333
-str r0, [r2, r1]//Configure gpio 0 to 7 to be inputs
 
-//Setup for port a
-ldr r1, =GPIO_PC_BASE
-mov r0, 0x2
-str r0, [r1]
-ldr r2, =GPIO_MODEH
-mov r0, 0x55555555
-str r0, [r2, r1]//Configure gpio 8 to 15 to be outputs
-//Not sure why I am doing this, just following instructions from compendium
-ldr r2, =GPIO_DOUT
-mov r0, 0xff
-str r0, [r2, r1]
-
-//Save some values that will be needed in the loop
-ldr r1, =GPIO_PA_BASE
-ldr r0, =GPIO_DOUT
-add r1, r1, r0//Port A DOUT register
-ldr r2, =GPIO_PC_BASE
-ldr r0, =GPIO_DIN
-add r2, r2, r0//Port C DIN register
-//Infinite loop that polls gpio for input each iteration
-loop:
-	//Check if pin 0 is pushed
-	and r3, r2, 0b00000001
-	cmp r3, 1
-	//If pin 0 is pushed, jump to pin0pushed
-	beq pin0pushed
-	//Turn off light 0
-	ldr r4, [r1]
-	and r4, r4, 0
-	b cont0
-	pin0pushed:
-		//Turn on light 0
-		ldr r4, [r1]
-		orr r4, r4, 0b00000001
-		str r1, [r4]
-	cont0:
-	b loop
