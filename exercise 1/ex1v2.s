c29d2768ad34afc669e9edbdf9ffeddf03223c3e
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2018-09-26 12:38:08 +0200

    Interrupt code that doesn't work

---
+++ exercise 1/ex1v2.s
@@ -0,0 +1,165 @@
+        .syntax unified
+	
+	      .include "efm32gg.s"
+
+	/////////////////////////////////////////////////////////////////////////////
+	//
+  // Exception vector table
+  // This table contains addresses for all exception handlers
+	//
+	/////////////////////////////////////////////////////////////////////////////
+	
+        .section .vectors
+	
+	      .long   stack_top               /* Top of Stack                 */
+	      .long   _reset                  /* Reset Handler                */
+	      .long   dummy_handler           /* NMI Handler                  */
+	      .long   dummy_handler           /* Hard Fault Handler           */
+	      .long   dummy_handler           /* MPU Fault Handler            */
+	      .long   dummy_handler           /* Bus Fault Handler            */
+	      .long   dummy_handler           /* Usage Fault Handler          */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* SVCall Handler               */
+	      .long   dummy_handler           /* Debug Monitor Handler        */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* PendSV Handler               */
+	      .long   dummy_handler           /* SysTick Handler              */
+
+	      /* External Interrupts */
+	      .long   dummy_handler
+	      .long   gpio_handler            /* GPIO even handler */
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   gpio_handler            /* GPIO odd handler */
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+
+	      .section .text
+
+	/////////////////////////////////////////////////////////////////////////////
+	//
+	// Reset handler
+  // The CPU will start executing here after a reset
+	//
+	/////////////////////////////////////////////////////////////////////////////
+
+	      .globl  _reset
+	      .type   _reset, %function
+        .thumb_func
+_reset:
+	//setup GPIO clock in CMU
+	ldr r1, =CMU_BASE
+	ldr r2, =CMU_HFPERCLKEN0
+	ldr r3, =CMU_HFPERCLKEN0_GPIO
+	mov r4, 1
+	lsl r4, r4, r3
+	orr r4, r4, r2
+	str r4, [r1, r2]
+	
+	//Cofigure GPIO to allow interrupts
+	ldr r1, =GPIO_BASE
+	mov r2, 0x22222222
+	str r2, [r1]
+	ldr r2, =GPIO_EXTIFALL
+	mov r3, 0xff
+	str r3, [r1, r2]
+	ldr r2, =GPIO_EXTIRISE
+	str r3, [r1, r2]
+	ldr r2, =GPIO_IEN
+	str r3, [r1, r2]
+	
+	//Setup port a
+	ldr r1, =GPIO_PA_BASE
+	mov r2, 0x2
+	str r2, [r1]
+	ldr r2, =GPIO_MODEH
+	mov r3, 0x55555555
+	str r3, [r1, r2]
+	//Turn off lights
+	ldr r2, =GPIO_DOUT
+	mov r3, 0xffffffff
+	str r3, [r1, r2]
+	
+	//Setup port c
+	ldr r1, =GPIO_PC_BASE
+	ldr r2, =GPIO_MODEL
+	mov r3, 0x33333333
+	str r3, [r1, r2]
+	ldr r2, =GPIO_DOUT
+	mov r3, 0xff
+	str r3, [r1, r2]
+	
+	//Clear interupt flags
+	ldr r1, =GPIO_BASE
+	ldr r2, [r1,#GPIO_IF]
+	str r2, [r1,#GPIO_IFC]
+	
+	//Setup NVIC
+	ldr r1, =ISER0
+	movw r2, 0x802
+	str r2, [r1]
+	loop:
+		b loop
+
+	/////////////////////////////////////////////////////////////////////////////
+	//
+  // GPIO handler
+  // The CPU will jump here when there is a GPIO interrupt
+	//
+	/////////////////////////////////////////////////////////////////////////////
+        .thumb_func
+gpio_handler:
+	ldr r1, =GPIO_PA_BASE
+	ldr r2, =GPIO_DOUT
+	mov r3, 0
+	str r3, [r1, r2]
+	
+	ldr r1, =GPIO_BASE
+	ldr r2, =GPIO_IF
+	ldr r3, =GPIO_IFC
+	ldr r4, [r1, r2]
+	str r4, [r3]
+	bx lr
+	
+	/////////////////////////////////////////////////////////////////////////////
+	
+        .thumb_func
+dummy_handler:  
+        b .  // do nothing
+
+
