a5ab03f4fbb3576f18728d583620c54cc1616484
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2018-09-26 22:05:30 +0200

    Interrupts now work

--- exercise 1/ex1v2.s
+++ exercise 1/ex1v2.s
@@ -82,40 +82,31 @@
 	      .type   _reset, %function
         .thumb_func
 _reset:
-	//setup GPIO clock in CMU
+	//enable GPIO clock
 	ldr r1, =CMU_BASE
 	ldr r2, =CMU_HFPERCLKEN0
-	ldr r3, =CMU_HFPERCLKEN0_GPIO
+	ldr r3, [r1, r2]
 	mov r4, 1
-	lsl r4, r4, r3
-	orr r4, r4, r2
-	str r4, [r1, r2]
-	
-	//Cofigure GPIO to allow interrupts
-	ldr r1, =GPIO_BASE
-	mov r2, 0x22222222
-	str r2, [r1]
-	ldr r2, =GPIO_EXTIFALL
-	mov r3, 0xff
-	str r3, [r1, r2]
-	ldr r2, =GPIO_EXTIRISE
-	str r3, [r1, r2]
-	ldr r2, =GPIO_IEN
+	ldr r5, =CMU_HFPERCLKEN0_GPIO
+	lsl r4, r4, r5
+	orr r3, r3, r4
 	str r3, [r1, r2]
 	
-	//Setup port a
+	//Enable LEDS for port A
 	ldr r1, =GPIO_PA_BASE
+	//Set high drive strenght
 	mov r2, 0x2
 	str r2, [r1]
+	//Set pins 8-15 to output
 	ldr r2, =GPIO_MODEH
 	mov r3, 0x55555555
 	str r3, [r1, r2]
-	//Turn off lights
+	//Turn off LEDS
 	ldr r2, =GPIO_DOUT
 	mov r3, 0xffffffff
 	str r3, [r1, r2]
 	
-	//Setup port c
+	//Enable port c buttons for input on pins 0-7
 	ldr r1, =GPIO_PC_BASE
 	ldr r2, =GPIO_MODEL
 	mov r3, 0x33333333
@@ -124,17 +115,29 @@ _reset:
 	mov r3, 0xff
 	str r3, [r1, r2]
 	
-	//Clear interupt flags
+	//Configure GPIO control registers to allow for interrupts
 	ldr r1, =GPIO_BASE
-	ldr r2, [r1,#GPIO_IF]
-	str r2, [r1,#GPIO_IFC]
+	ldr r2, =GPIO_EXTIPSELL
+	mov r3, 0x22222222
+	str r3, [r1, r2]
+	mov r3, 0xff
+	ldr r2, =GPIO_EXTIRISE
+	str r3, [r1, r2]
+	ldr r2, =GPIO_EXTIFALL
+	str r3, [r1, r2]
+	ldr r2, =GPIO_IEN
+	str r3, [r1, r2]
 	
-	//Setup NVIC
+	//Enable NVIC
 	ldr r1, =ISER0
-	movw r2, 0x802
+	ldr r2, =0x802
 	str r2, [r1]
-	loop:
-		b loop
+	
+	//Enable deep sleep and exit to sleep
+	ldr r1, =SCR
+	mov r2, 0b110
+	str r2, [r1]
+	wfi
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
@@ -142,24 +145,27 @@ _reset:
   // The CPU will jump here when there is a GPIO interrupt
 	//
 	/////////////////////////////////////////////////////////////////////////////
+
         .thumb_func
 gpio_handler:
-	ldr r1, =GPIO_PA_BASE
-	ldr r2, =GPIO_DOUT
-	mov r3, 0
-	str r3, [r1, r2]
-	
 	ldr r1, =GPIO_BASE
 	ldr r2, =GPIO_IF
 	ldr r3, =GPIO_IFC
 	ldr r4, [r1, r2]
-	str r4, [r3]
-	bx lr
+	str r4, [r1, r3]
 	
+	ldr r1, =GPIO_PC_BASE
+	ldr r2, =GPIO_DIN
+	ldr r3, [r1, r2]
+	mov r4, 8
+	lsl r3, r3, r4
+	ldr r1, =GPIO_PA_BASE
+	ldr r2, =GPIO_DOUT
+	str r3, [r1, r2]
+	bx lr	
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
 dummy_handler:  
         b .  // do nothing
-
 
