6f56b9ec83f4314e95c867af7609b58b2f28108f
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2018-09-27 15:49:59 +0200

    Added energy optimization. I think the program is done now

--- exercise 1/ex1v2.s
+++ exercise 1/ex1v2.s
@@ -82,7 +82,30 @@
 	      .type   _reset, %function
         .thumb_func
 _reset:
+	bl gpio_clock_setup
+	
+	bl gpio_led_setup
+	
+	bl gpio_button_setup
+	
+	bl gpio_interrupt_setup
+	
+	bl nvic_setup
+	
+	bl sleep_setup
+	
+	bl energy_optimization_low_drive_strength
+	
+	bl energy_optimization_turn_off_sram
+	
+	//bl energy_optimization_adjust_peripheral_clock
+	
+	bl energy_optimization_adjust_core_clock
+	wfi
+	
+	
 	//enable GPIO clock
+gpio_clock_setup:
 	ldr r1, =CMU_BASE
 	ldr r2, =CMU_HFPERCLKEN0
 	ldr r3, [r1, r2]
@@ -91,12 +114,11 @@ _reset:
 	lsl r4, r4, r5
 	orr r3, r3, r4
 	str r3, [r1, r2]
+	bx lr
 	
 	//Enable LEDS for port A
+gpio_led_setup:
 	ldr r1, =GPIO_PA_BASE
-	//Set high drive strenght
-	mov r2, 0x2
-	str r2, [r1]
 	//Set pins 8-15 to output
 	ldr r2, =GPIO_MODEH
 	mov r3, 0x55555555
@@ -105,8 +127,10 @@ _reset:
 	ldr r2, =GPIO_DOUT
 	mov r3, 0xffffffff
 	str r3, [r1, r2]
+	bx lr
 	
 	//Enable port c buttons for input on pins 0-7
+gpio_button_setup:
 	ldr r1, =GPIO_PC_BASE
 	ldr r2, =GPIO_MODEL
 	mov r3, 0x33333333
@@ -114,8 +138,10 @@ _reset:
 	ldr r2, =GPIO_DOUT
 	mov r3, 0xff
 	str r3, [r1, r2]
+	bx lr
 	
 	//Configure GPIO control registers to allow for interrupts
+gpio_interrupt_setup:
 	ldr r1, =GPIO_BASE
 	ldr r2, =GPIO_EXTIPSELL
 	mov r3, 0x22222222
@@ -127,17 +153,56 @@ _reset:
 	str r3, [r1, r2]
 	ldr r2, =GPIO_IEN
 	str r3, [r1, r2]
+	bx lr
 	
 	//Enable NVIC
+nvic_setup:
 	ldr r1, =ISER0
 	ldr r2, =0x802
 	str r2, [r1]
+	bx lr
 	
 	//Enable deep sleep and exit to sleep
+sleep_setup:
 	ldr r1, =SCR
 	mov r2, 0b110
 	str r2, [r1]
-	wfi
+	bx lr
+	
+	//Set low drive strength for leds
+energy_optimization_low_drive_strength:
+	ldr r1, =GPIO_PA_BASE
+	mov r2, 1
+	str r2, [r1]
+	bx lr
+	
+	//Turn off sram
+energy_optimization_turn_off_sram:
+	ldr r1, =EMU_BASE
+	mov r2, 0x4//EMU_MEMCTRL offset
+	mov r3, 0x7
+	str r3, [r1, r2]
+	bx lr
+	
+	//Adjust peripheral clock
+energy_optimization_adjust_peripheral_clock:
+	ldr r1, =CMU_BASE
+	mov r2, 0x8//CMU_HFPERCLKDIV offset
+	ldr r3, [r1, r2]//The value that gets loaded in here contins an enable bit
+	mov r4, 5//Value I want written to CMU_HFPERCLKDIV
+	orr r3, r3, r4//Need to ensure that I don't overwrite the enable bit
+	str r3, [r1, r2]
+	bx lr
+	
+	//Adjust core clock
+energy_optimization_adjust_core_clock:
+	ldr r1, =CMU_BASE
+	mov r2, 0x4//CMU_HFCORECLKDIV offset
+	ldr r3, [r1, r2]//The value that gets loaded in here contins a bit that shouldn't get overwritten
+	mov r4, 2//Value I want written to CMU_HFCORECLKDIV
+	orr r3, r3, r4//Need to ensure that I don't overwrite the bit
+	str r3, [r1, r2]
+	bx lr
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
